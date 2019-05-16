1a4a2dc24040d5a0f1674fd32d936d7439b049c3
Eugio <47667122+ejhalpin@users.noreply.github.com>
2019-05-16 14:59:20 -0400

    added space, backspace (non-functioning), and shift keys. Fixed bugs in DOM events

--- assets/javascript/engine.js
+++ assets/javascript/engine.js
@@ -27,13 +27,45 @@ let keys = [
   "m" //row 3
 ];
 var keyboard = document.getElementById("keyboard");
-function doLayout() {
-  for (var i = 0; i < keys.length; i++) {
-    var key = document.createElement("div");
-    key.style.gridArea = keys[i];
-    key.textContent = keys[i];
-    keyboard.appendChild(key);
-  }
+var target = document.createElement("div");
+document.body.appendChild(target);
+
+for (var i = 0; i < keys.length; i++) {
+  var key = document.createElement("div");
+  key.id = keys[i];
+  key.style.gridArea = keys[i];
+  key.textContent = keys[i];
+  key.className = "key";
+  // key.addEventListener("click", printKey(keys[i]));
+  keyboard.appendChild(key);
 }
 
-doLayout();
+for (var j = 0; j < keys.length; j++) {
+  document.getElementById(keys[j]).onclick = function() {
+    printKey(this.textContent);
+  };
+}
+
+function printKey(letter) {
+  var text = letter;
+  if (uppercase) {
+    text = letter.toUpperCase();
+  }
+  console.log(text);
+}
+
+var uppercase = false;
+
+function swapCase() {
+  for (var i = 0; i < keys.length; i++) {
+    var key = document.getElementById([keys[i]]);
+    if (uppercase) {
+      key.classList.add("lower");
+      key.classList.remove("upper");
+    } else {
+      key.classList.remove("lower");
+      key.classList.add("upper");
+    }
+  }
+  uppercase = !uppercase;
+}
